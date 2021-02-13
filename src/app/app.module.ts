a2a24732eb7883dbe3a71506625e6e1ef86751f5
thakur24 <thakurdhruv200@gmail.com>
2021-02-13 08:36:04 +0530

    my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FreeComponent } from './free/free.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FreeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/free/free.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FreeComponent } from './free.component';
+
+describe('FreeComponent', () => {
+  let component: FreeComponent;
+  let fixture: ComponentFixture<FreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FreeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
